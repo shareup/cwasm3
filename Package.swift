da60d7f032fd5be33053d51eb0d855b95ea163f1
Anthony Drendel <atdrendel@users.noreply.github.com>
2021-04-29 22:29:59 +0200

    Update to Swift 5.3 and support app extensions (#10)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.2
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -13,9 +12,17 @@ let package = Package(
     dependencies: [],
     targets: [
         .target(
-            name: "CWasm3"),
+            name: "CWasm3",
+            cSettings: [.define("APPLICATION_EXTENSION_API_ONLY", to: "YES")]),
         .testTarget(
             name: "CWasm3Tests",
-            dependencies: ["CWasm3"]),
+            dependencies: ["CWasm3"],
+            exclude: [
+                "Resources/add.wat",
+                "Resources/constant.wat",
+                "Resources/fib64.wat",
+                "Resources/imported-add.wat",
+                "Resources/memory.wat",
+            ]),
     ]
 )
